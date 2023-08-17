09a3b92258075fee34eb6a5e9fae8250efbf15d3
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-08-17 19:20:22 +1200

    docs: Updated docstrings for message class
    Fully finished adding meaningful docstrings to this class

--- src/console/message.py
+++ src/console/message.py
@@ -1,27 +1,27 @@
-"""Summary
-"""
+"""The module provides a way for the user to know whether the value they are
+trying to add is valid or not."""
 from __future__ import annotations
 
 from enum import Enum
 
 
 class Message(Enum):
-    """Summary.
+    """Message allows for exceptions to be displayed to the terminal.
 
     Attributes:
-        COMMON_VALUE_EXCEPTION (str): Description
-        NULL_VALUE_EXCEPTION (str): Description
+        COMMON_VALUE_EXCEPTION (str): Exception occurs when value is not unique.
+        NULL_VALUE_EXCEPTION (str): Exception occurs when value is null.
     """
 
     NULL_VALUE_EXCEPTION = "{value} IS NULL NODE"
     COMMON_VALUE_EXCEPTION = "NODE {value} IS ALREADY IN THE TREE"
 
-    def print(message: str, value: int):
-        """Summary.
+    def print(self, message: str, value: int):
+        """Print the message to the console.
 
         Args:
-            message (str): Description
-            value (int): Description
+            message (str): The message or exception to be displayed to the console.
+            value (int): The invalid value.
         """
 
         print(message.value.format(value=value), "\n")
